1f27618346fb7114f69518c3758e2056b0db7843
Constance Darlington <constancedarlington13@gmail.com>
2020-01-27 15:14:46 -0700

    test for hw2

---
+++ Homework 2/script.js
@@ -0,0 +1,19 @@
+// I am writing something to the console
+console.log("Hi in external file.");
+
+
+//o 3 of your favorite websites
+
+console.log("Instagram");
+console.log("Tik Tok");
+console.log("Facebook");
+
+
+//o 3 of your favorite artists
+console.log("Mindy Kaling");
+console.log("John Singer Sargent");
+console.log("John Mulaney");
+//o 3 of your favorite games
+console.log("Go Fish");
+console.log("Uno");
+console.log("Sorry");
